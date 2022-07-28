4c0135a9941cd31a2e2799be79fb937dd680e817
marinamangia <marina.mangia@br.ey.com>
2022-07-28 15:34:45 -0300

    update

--- Job_automation_with_selenium.py
+++ Job_automation_with_selenium.py
@@ -1,18 +1,3 @@
-<<<<<<< HEAD
-from selenium import webdriver
-from selenium.webdriver.common.by import By
-
-driver = webdriver.Chrome()
-
-get_google = driver.get('https://www.google.com')
-barra_pesquisa = driver.find_element(By.XPATH,'/html/body/div[1]/div[3]/form/div[1]/div[1]/div[1]/div/div[2]/input')
-barra_pesquisa.send_keys('ciencia de dados vagas\n')
-
-# clicando na pesquisa do linkedin
-resultado_linkedin = driver.find_element(By.CLASS_NAME,'LC20lb MBeuO DKV0Md')
-resultado_linkedin.click()
-
-=======
 from selenium import webdriver
 from selenium.webdriver.common.by import By
 from selenium.webdriver.support.ui import WebDriverWait
@@ -38,11 +23,3 @@ for i in resultados:
     lista_descricoes.append(descricao.text)
 
 
-
-
-<<<<<<< HEAD
->>>>>>> 94b5bc6 (add lista descricoes)
-=======
-
-
->>>>>>> 3790044 (add lista descricoes)
