4846791db4a9d93a50fa47d6ca48adac0471975c
marinamangia <marina.mangia@br.ey.com>
2022-07-28 15:11:02 -0300

    add lista descricoes

--- Job_automation_with_selenium.py
+++ Job_automation_with_selenium.py
@@ -1,3 +1,4 @@
+<<<<<<< HEAD
 from selenium import webdriver
 from selenium.webdriver.common.by import By
 
@@ -11,3 +12,32 @@ barra_pesquisa.send_keys('ciencia de dados vagas\n')
 resultado_linkedin = driver.find_element(By.CLASS_NAME,'LC20lb MBeuO DKV0Md')
 resultado_linkedin.click()
 
+=======
+from selenium import webdriver
+from selenium.webdriver.common.by import By
+from selenium.webdriver.support.ui import WebDriverWait
+from selenium.webdriver.support import expected_conditions as EC
+
+driver = webdriver.Chrome()
+driver.maximize_window()
+driver.get('https://www.google.com')
+barra_pesquisa = driver.find_element(By.XPATH,'/html/body/div[1]/div[3]/form/div[1]/div[1]/div[1]/div/div[2]/input')
+barra_pesquisa.send_keys('ciencia de dados vagas\n')
+
+# clicando na pesquisa do linkedin
+WebDriverWait(driver, 20).until(EC.visibility_of_element_located((By.XPATH, '//*[@id="rso"]/div[3]/div/'
+                                                                            'div[1]/div/a/h3'))).click()
+
+descricao = driver.find_element(By.CLASS_NAME,'show-more-less-html__markup')
+resultados = driver.find_elements(By.CLASS_NAME,'base-card__full-link')
+
+lista_descricoes=[]
+for i in resultados:
+    i.click()
+    descricao = driver.find_element(By.CLASS_NAME,'show-more-less-html__markup')
+    lista_descricoes.append(descricao.text)
+
+
+
+
+>>>>>>> 94b5bc6 (add lista descricoes)
